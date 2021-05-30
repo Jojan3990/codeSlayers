75e30eacd8d6fe819d1a2971b88ecf34099b886a
Jojan3990 <raijozan2443@gmail.com>
2021-05-30 20:24:12 +0545

    this is codeSlayers project team

---
+++ src/main/java/codeSlayers/freeBlogShoot/FreeBlogShootApplication.java
@@ -0,0 +1,13 @@
+package codeSlayers.freeBlogShoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeBlogShootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeBlogShootApplication.class, args);
+	}
+
+}
